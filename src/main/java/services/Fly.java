93cda37c67dc508e6dfac2fccdc1b2a7e2638aa7
Samy Chaabi <xhackax47@gmail.com>
2018-10-03 16:28:58 +0200

    Issues Fixs

--- src/main/java/services/Fly.java
+++ src/main/java/services/Fly.java
@@ -8,7 +8,7 @@ import dao.ServiceJPA;
 import model.Menus;
 
 public class Fly {
-	
+
 	public static void start() {
 		final Scanner sc = new Scanner(System.in);
 		int selection = 0;
@@ -17,63 +17,62 @@ public class Fly {
 
 		do {
 			Menus.afficheMenuGeneral();
-			String s=sc.nextLine();
-			selection=Integer.parseInt(s);
-			 
-			search:
-			switch (selection) {
+			String s = sc.nextLine();
+			selection = Integer.parseInt(s);
+
+			search: switch (selection) {
 			case 1:
 				do {
 					Menus.afficheMenuVols();
-					String s2=sc.nextLine();
-					flightSelection=Integer.parseInt(s2);
-					
-					switch(flightSelection) {
+					String s2 = sc.nextLine();
+					flightSelection = Integer.parseInt(s2);
+
+					switch (flightSelection) {
 					case 1:
-						
+
 						try {
 							ServiceJPA.createFly(sc);
 						} catch (InterruptedException | ParseException e) {
 							e.printStackTrace();
 						}
-							
+
 						break;
-						
+
 					case 2:
 						Service.listFlys();
 						break;
-						
+
 					case 3:
 						ServiceJPA.displayFlysByNum(sc);
 						break;
-						
+
 					case 4:
 						break;
-						
-					case 5:			
+
+					case 5:
 						System.out.println("Voulez-vous retourner au menu général ? (O ou N)");
 						String returnBack = sc.nextLine();
-						if(returnBack == "O") {
-	
+						if (returnBack == "O") {
+
 							break search;
-						}else {
-						break;
+						} else {
+							break;
 						}
 					default:
 						System.out.println("Choix inconnu");
 						break;
 					}
-				
-				}while(flightSelection != 5);
+
+				} while (flightSelection != 5);
 
 				break;
 			case 2:
-				do {		
+				do {
 					Menus.afficheMenuReservations();
-					String s3=sc.nextLine();
+					String s3 = sc.nextLine();
 					reservationSelection = Integer.parseInt(s3);
-					
-					switch(reservationSelection) {
+
+					switch (reservationSelection) {
 					case 1:
 						try {
 							ServiceJPA.createReservation(sc);
@@ -87,7 +86,7 @@ public class Fly {
 					case 3:
 						System.out.println("Entrer le numéro de réservation à annuler : ");
 						String numRes = sc.nextLine();
-						
+
 						ServiceJPA.deleteReservation(numRes);
 						System.out.println("Reservation N°" + numRes + " supprimée");
 						break;
@@ -96,7 +95,7 @@ public class Fly {
 					case 5:
 						System.out.println("Voulez-vous retourner au menu général ? (O ou N)");
 						char returnBack = sc.next().charAt(0);
-						if(returnBack == 'O') {
+						if (returnBack == 'O') {
 							break search;
 						}
 						break;
@@ -104,8 +103,8 @@ public class Fly {
 						System.out.println("Choix inconnu");
 						break;
 					}
-				
-				}while(flightSelection != 5);
+
+				} while (flightSelection != 5);
 
 				break;
 			case 3:
@@ -115,11 +114,8 @@ public class Fly {
 				System.out.println("Choix inconnu");
 			}
 
-			
-			
-
 		} while (selection != 3);
-		
+
 		sc.close();
 		System.out.flush();
 		System.out.close();
